fe794ccee5b59d9258a1d4472522747bce9ffdc2
sophia paulin <sobiyamary622@gmail.com>
2024-07-08 07:17:17 +0530

    updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,156 +1,225 @@
-import React, { Component } from "react"; 
-// import "bootstrap/dist/css/bootstrap.css"; 
-// import Container from "react-bootstrap/Container"; 
-// import Row from "react-bootstrap/Row"; 
-// import Col from "react-bootstrap/Col"; 
-// import Button from "react-bootstrap/Button"; 
-// import InputGroup from "react-bootstrap/InputGroup"; 
-// import FormControl from "react-bootstrap/FormControl"; 
-// import ListGroup from "react-bootstrap/ListGroup"; 
-  
-class App extends Component { 
-    constructor(props) { 
-        super(props); 
-  
-        // Setting up state 
-        this.state = { 
-            userInput: "", 
-            list: [], 
-        }; 
-    } 
-  
-    // Set a user input value 
-    updateInput(value) { 
-        this.setState({ 
-            userInput: value, 
-        }); 
-    } 
-  
-    // Add item if user input in not empty 
-    addItem() { 
-        if (this.state.userInput !== "") { 
-            const userInput = { 
-                // Add a random id which is used to delete 
-                id: Math.random(), 
-  
-                // Add a user value to list 
-                value: this.state.userInput, 
-            }; 
-  
-            // Update list 
-            const list = [...this.state.list]; 
-            list.push(userInput); 
-  
-            // reset state 
-            this.setState({ 
-                list, 
-                userInput: "", 
-            }); 
-        } 
-    } 
-  
-    // Function to delete item from list use id to delete 
-    deleteItem(key) { 
-        const list = [...this.state.list]; 
-  
-        // Filter values and leave value which we need to delete 
-        const updateList = list.filter((item) => item.id !== key); 
-  
-        // Update list in state 
-        this.setState({ 
-            list: updateList, 
-        }); 
-    } 
-  
-    editItem = (index) => { 
-      const todos = [...this.state.list]; 
-      const editedTodo = prompt('Edit the todo:'); 
-      if (editedTodo !== null && editedTodo.trim() !== '') { 
-        let updatedTodos = [...todos] 
-        updatedTodos[index].value= editedTodo 
-        this.setState({ 
-          list: updatedTodos, 
-      }); 
-      } 
-    } 
-  
-    render() { 
-        return ( 
-            <Container> 
-                <Row 
-                    style={{ 
-                        display: "flex", 
-                        justifyContent: "center", 
-                        alignItems: "center", 
-                        fontSize: "3rem", 
-                        fontWeight: "bolder", 
-                    }} 
-                > 
-                    TODO LIST 
-                </Row> 
-  
-                <hr /> 
-                <Row> 
-                    <Col md={{ span: 5, offset: 4 }}> 
-                        <InputGroup className="mb-3"> 
-                            <FormControl 
-                                placeholder="add item . . . "
-                                size="lg"
-                                value={this.state.userInput} 
-                                onChange={(item) => 
-                                    this.updateInput(item.target.value) 
-                                } 
-                                aria-label="add something"
-                                aria-describedby="basic-addon2"
-                            /> 
-                            <InputGroup> 
-                                <Button 
-                                    variant="dark"
-                                    className="mt-2"
-                                    onClick={() => this.addItem()} 
-                                > 
-                                    ADD 
-                                </Button> 
-                            </InputGroup> 
-                        </InputGroup> 
-                    </Col> 
-                </Row> 
-                <Row> 
-                    <Col md={{ span: 5, offset: 4 }}> 
-                        <ListGroup> 
-                            {/* map over and print items */} 
-                            {this.state.list.map((item, index) => { 
-                                return ( 
-                                  <div key = {index} >  
-                                    <ListGroup.Item 
-                                        variant="dark"
-                                        action 
-                                        style={{display:"flex", 
-                                                justifyContent:'space-between'
-                                      }} 
-                                    > 
-                                        {item.value} 
-                                        <span> 
-                                        <Button style={{marginRight:"10px"}} 
-                                        variant = "light"
-                                        onClick={() => this.deleteItem(item.id)}> 
-                                          Delete 
-                                        </Button> 
-                                        <Button variant = "light"
-                                        onClick={() => this.editItem(index)}> 
-                                          Edit 
-                                        </Button> 
-                                        </span> 
-                                    </ListGroup.Item> 
-                                  </div> 
-                                ); 
-                            })} 
-                        </ListGroup> 
-                    </Col> 
-                </Row> 
-            </Container> 
-        ); 
-    } 
-} 
-  
-export default App; 
+var todoTitleInput = document.querySelector('#todo-title-input');
+var todoAddBtn = document.querySelector('#todo-add-btn');
+var todoList = document.querySelector('#todo-list')
+var todos = JSON.parse(localStorage.getItem('todos'));
+
+// Constructor Class
+function todoItemTemplate(title, done = false){
+    this.title = title;
+    this.done = done;
+};
+
+// messages DOM Selection for the manipulation later in the javascript document
+var startEditingMsg = document.getElementById('start-editing');
+var savedMsg = document.getElementById('saved');
+var deletedMsg = document.getElementById('deleted');
+var startEditingMsgST, savedMsgST, deletedMsgST;
+function startEditingMsgSTF(){
+    startEditingMsgST = setTimeout(function(){
+        startEditingMsg.style.opacity = '0';
+    }, 8000);
+}
+function savedMsgSTF(){
+    savedMsgST = setTimeout(function(){
+        savedMsg.style.opacity = '0';
+    }, 5000);
+}
+function deletedMsgSTF(){
+    deletedMsgST = setTimeout(function(){
+        deletedMsg.style.opacity = '0';
+    }, 5000);
+}
+function startEditingMsgCTF(){
+    clearTimeout(startEditingMsgST);
+}
+function savedMsgCTF(){
+    clearTimeout(savedMsgST);
+}
+function deletedMsgCTF(){
+    clearTimeout(deletedMsgST);
+}
+
+
+
+if(todos !== null){
+    todos.forEach(todo => {
+        addListItem(todo['title']);
+        if(todo['done']){
+            todoList.children[todos.indexOf(todo)].classList.add('done-assistance');
+            todoList.children[todos.indexOf(todo)].firstElementChild.classList.add('done');
+        }
+    });
+}else if(todos === null){
+    todos = new Array();
+    localStorage.setItem('todos', JSON.stringify(todos));
+};
+
+// Add event listeners 
+todoAddBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+    if(todoTitleInput.value !== ""){
+        var todos = JSON.parse(localStorage.getItem('todos'));
+        todos.push(new todoItemTemplate(todoTitleInput.value));
+        localStorage.setItem('todos', JSON.stringify(todos));
+        addListItem(todoTitleInput.value);
+    }
+    todoTitleInput.value = "";
+})
+
+// console.log(todos);
+
+function addListItem(todoTitleValue){
+        // creating the list item
+        var todoListItem = document.createElement('li');
+        todoListItem.classList.add('todo-list-item');
+
+
+        // creating the todo list title
+        var todoListTitle = document.createElement('h1');
+        todoListTitle.classList.add('todo-list-title');
+        todoListItem.appendChild(todoListTitle);
+        todoListTitle.innerText = todoTitleValue;
+        
+
+        // creating div for buttons
+        var listBtns = document.createElement('div');
+        listBtns.classList.add('list-btns');
+
+        var editBtn = document.createElement('button');
+        editBtn.classList.add('edit-btn');
+        listBtns.appendChild(editBtn);
+        editBtn.innerHTML = '<i class="fa fa-edit"></i>';
+
+        var updateBtn = document.createElement('button');
+        updateBtn.classList.add('update-btn');
+        listBtns.appendChild(updateBtn);
+        updateBtn.innerHTML = '<i class="fa fa-save"></i>';
+
+        var doneBtn = document.createElement('button');
+        doneBtn.classList.add('done-btn');
+        listBtns.appendChild(doneBtn);
+        doneBtn.innerHTML = '<i class="fa fa-check"></i>';
+
+        var dltBtn = document.createElement('button');
+        dltBtn.classList.add('dlt-btn');
+        listBtns.appendChild(dltBtn);
+        dltBtn.innerHTML = '<i class="fa fa-trash"></i>';
+
+        todoListItem.appendChild(listBtns);
+
+        // appending the entire list item inside the todo list
+        todoList.appendChild(todoListItem);
+
+        // console.log(todoListItem);
+
+};
+todoList.addEventListener('click', (e)=> {
+    if(e.target.classList[0] == "edit-btn"){
+        // if done is true, cant edit
+        var reqIndex;
+        for (var i = 0; i < todoList.children.length; i++){
+        if(e.target.parentElement.parentElement === todoList.children[i]){
+            reqIndex = i;
+            break;
+            }
+        }
+        var todos = JSON.parse(localStorage.getItem('todos'));
+        if(todos[reqIndex]['done']){
+            window.alert('Error! Can\'t edit if done');
+        }else{
+            e.target.nextElementSibling.style.display = "flex";
+            e.target.style.display = "none";
+            e.target.parentElement.parentElement.firstElementChild.contentEditable = true;
+            e.target.nextElementSibling.nextElementSibling.disabled = true;
+            // start editing message alert
+            startEditingMsg.style.opacity = '1';
+            savedMsg.style.opacity = '0';
+            deletedMsg.style.opacity = '0';
+            startEditingMsgCTF();
+            savedMsgCTF();
+            deletedMsgCTF();
+            startEditingMsgSTF();
+        }
+    };
+    if(e.target.classList[0] == "update-btn"){
+        if(e.target.parentElement.parentElement.firstElementChild.innerText !== ""){
+            e.target.previousElementSibling.style.display = "flex";
+            e.target.style.display = "none";
+            e.target.parentElement.parentElement.firstElementChild.contentEditable = false;
+            e.target.nextElementSibling.disabled = false;
+
+            // localStorage work
+            var reqIndex;
+            for (var i = 0; i < todoList.children.length; i++){
+                if(e.target.parentElement.parentElement === todoList.children[i]){
+                    reqIndex = i;
+                    break;
+                }
+            }
+            var todos = JSON.parse(localStorage.getItem('todos'));
+            // localStorage.removeItem('todos');
+            todos[reqIndex] = new todoItemTemplate(e.target.parentElement.parentElement.firstElementChild.innerText);
+            localStorage.setItem('todos', JSON.stringify(todos));
+
+
+
+            // saved message alert
+            startEditingMsg.style.opacity = '0';
+            savedMsg.style.opacity = '1';
+            deletedMsg.style.opacity = '0';
+            startEditingMsgCTF();
+            savedMsgCTF();
+            deletedMsgCTF();
+            savedMsgSTF();
+        }
+        if(e.target.parentElement.parentElement.firstElementChild.innerText == ""){
+            window.alert('Error! You can\'t leave the space empty');
+        }
+    }
+    if(e.target.classList[0] == "done-btn"){
+        e.target.parentElement.parentElement.firstElementChild.classList.toggle('done');
+        e.target.parentElement.parentElement.classList.toggle('done-assistance');
+
+        // localStorage work
+        var reqIndex;
+        for (var i = 0; i < todoList.children.length; i++){
+            if(e.target.parentElement.parentElement === todoList.children[i]){
+                reqIndex = i;
+                break;
+            }
+        }
+        var todos = JSON.parse(localStorage.getItem('todos'));
+        if (todos[reqIndex]['done']){
+            todos[reqIndex] = new todoItemTemplate(e.target.parentElement.parentElement.firstElementChild.innerText, false);
+        }else{
+            todos[reqIndex] = new todoItemTemplate(e.target.parentElement.parentElement.firstElementChild.innerText, true);
+        };
+        localStorage.setItem('todos', JSON.stringify(todos));
+    }
+    if(e.target.classList[0] == "dlt-btn"){
+
+        // localStorage work
+        for (var i = 0; i < todoList.children.length; i++){
+            if(e.target.parentElement.parentElement === todoList.children[i]){
+                reqIndex = i;
+                break;
+            };
+        };
+        var todos = JSON.parse(localStorage.getItem('todos'));
+        todos.splice(reqIndex, 1);
+        localStorage.setItem('todos', JSON.stringify(todos));
+
+        //real function
+        e.target.parentElement.parentElement.remove();
+
+        // delete message alert
+        startEditingMsg.style.opacity = '0';
+        savedMsg.style.opacity = '0';
+        deletedMsg.style.opacity = '1';
+        startEditingMsgCTF();
+        savedMsgCTF();
+        deletedMsgCTF();
+        deletedMsgSTF();
+    }
+
+});
